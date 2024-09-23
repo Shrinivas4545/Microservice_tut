0ced87ad707b964f0a793bf3701546012e1058eb
Shrinivas Patil <shripatil430@gmail.com>
2024-09-23 19:49:16 +0530

    more on @Value and @ConfigationProperties

--- javaBrains/Microservice_javabrains/Step03/spring-boot-config/src/main/java/com/shrinivas/spring_boot_config/Controller.java
+++ javaBrains/Microservice_javabrains/Step03/spring-boot-config/src/main/java/com/shrinivas/spring_boot_config/Controller.java
@@ -1,5 +1,9 @@
 package com.shrinivas.spring_boot_config;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-	@Value("${my.fullGreetDesc}")
+//	@Value("${my.fullGreetDesc:default}")
+	@Value("${my.fullGreetDesc:default value}")  // takes default value when my.fullGreetDesc is not there in application.properties
 	private String greetMsg;
+	
+	@Value("${my.list}")
+	private List<String> list;
+	
+	@Value("#{${dbValues}}")
+	private Map<String, String> map;
+	
+	@Autowired
+	private DbConnection dbConnection;
+	
 	
 	@GetMapping("/greet")
 	public String getGreeting() {
-		return greetMsg;
+		return greetMsg+map;
+	}
+	
+	@GetMapping("/db")
+	public String getDbDetails() {
+		return dbConnection.getConnection()+dbConnection.getHost()+dbConnection.getPort();
 	}
 }
