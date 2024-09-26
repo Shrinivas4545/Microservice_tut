0a4a50bce20c7a012178eb0e52125bcb4e04c06c
Shrinivas Patil <shripatil430@gmail.com>
2024-09-26 16:01:57 +0530

    Config server, client

--- javaBrains/Microservice_javabrains/Step03/spring-boot-config/src/main/java/com/shrinivas/spring_boot_config/SpringBootConfigApplication.java
+++ javaBrains/Microservice_javabrains/Step03/spring-boot-config/src/main/java/com/shrinivas/spring_boot_config/SpringBootConfigApplication.java
@@ -2,6 +2,7 @@ package com.shrinivas.spring_boot_config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 public class SpringBootConfigApplication {
